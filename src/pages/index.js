51cec32b29c06da9abf7a90de7562d0fd61c670d
Sebastian Crossa <crossasebastian@gmail.com>
2018-12-16 14:43:58 -0700

    Added project images and started work on header setcion

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,14 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  	<div className="hero">
+  		<div className="hero-group">
+  			<h1>Learn to design and code React apps</h1>
+   			<p>Complete course about the best tools and design systems. 
+   			Prototype and build apps with React and Swift</p>
+    		<Link to="/page-2/">Go to page 2</Link>
+  		</div>
+  	</div>
   </div>
 )
 
