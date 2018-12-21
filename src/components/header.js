236c89e072e49a7ffef6b6cda20c311cc009406e
Sebastian Crossa <crossasebastian@gmail.com>
2018-12-20 22:20:24 -0700

    Converted the nav component into a stateful component, adding a clean transition

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,43 @@
 import React from 'react'
 import Link from 'gatsby-link'
-import './header.css'
+import './Header.css'
 
-const Header = ({ siteTitle }) => (
-  <div className='header'>
-    <div className='header-group'>
-      <Link to="/"><img src={require('../img/logo-designcode.svg')} width="30" /></Link>
-      <Link to="/courses">Courses</Link>
-      <Link to="/downloads">Downloads</Link>
-      <Link to="/workshops">Workshops</Link>
-      <Link to="/buy"><button>BUY</button></Link>
-    </div>
-  </div>
-)
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      hasScrolled: false
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll)
+  }
+
+  handleScroll = (event) => {
+    const scrollTop = window.pageYOffset
+
+    if (scrollTop > 50) {
+      this.setState({ hasScrolled: true })
+    } else {
+      this.setState({ hasScrolled: false })
+    }
+  }
+
+  render() {
+    return (
+      <div className={this.state.hasScrolled ? 'header header-scrolled' : 'header'}>
+        <div className='header-group'>
+          <Link to="/"><img src={require('../img/logo-designcode.svg')} width="30" /></Link>
+          <Link to="/courses">Courses</Link>
+          <Link to="/downloads">Downloads</Link>
+          <Link to="/workshops">Workshops</Link>
+          <Link to="/buy"><button>BUY</button></Link>
+        </div>
+      </div>
+    )
+  }
+}
 
 export default Header
